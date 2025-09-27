58361d820492ffdeb5da62fa3288750c6953b6ab
Lukáš Úradník <82445388+Krypt0niT@users.noreply.github.com>
2025-09-27 19:26:08 +0200

    Tokenizer: Added token types from assignment.

---
+++ tokenizer/token.h
@@ -0,0 +1,61 @@
+// token.h
+#ifndef TOKEN_H
+#define TOKEN_H
+
+// Typy tokenov
+typedef enum {
+    // --- Klucove slova ---
+    TOKEN_CLASS,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_IS,
+    TOKEN_NULL,
+    TOKEN_RETURN,
+    TOKEN_VAR,
+    TOKEN_WHILE,
+    TOKEN_IFJ,
+    TOKEN_STATIC,
+    TOKEN_IMPORT,
+    TOKEN_FOR,
+    TOKEN_NUM,
+    TOKEN_STRING,
+    TOKEN_NULL_LITERAL,
+
+    // --- Identifikator ---
+    TOKEN_ID,
+
+    // --- Globalna premenna
+    TOKEN_GLOBAL_VAR,
+
+    // --- Literály ---
+    TOKEN_INT_LITERAL,
+    TOKEN_HEX_LITERAL,
+    TOKEN_STRING_LITERAL,
+    TOKEN_FLOAT_LITERAL,
+
+    // --- Aritmeticke operatory ---
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_MUL,
+    TOKEN_DIV,
+
+    // --- Relacne operatory ---
+    TOKEN_LT,       // <
+    TOKEN_GT,       // >
+    TOKEN_LE,       // <=
+    TOKEN_GE,       // >=
+    TOKEN_EQ,       // ==
+    TOKEN_NE,       // !=
+
+    // --- KOniec riadku ---
+    TOKEN_LINE_END,
+
+} TokenType;
+
+
+typedef struct {
+    TokenType type;
+    char* value; // lexema zo zdroju
+} Token;
+
+#endif
