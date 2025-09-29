5a54f94d61a1e4b92614ca6dcef00b7800180346
Krypt0niT <k269692@gmail.com>
2025-09-29 02:41:17 +0200

    Utilities: Added Trim utility.

---
+++ utilities/trim.h
@@ -0,0 +1,7 @@
+/**
+ * @brief Oreže biele znaky zo začiatku a konca reťazca.
+ *
+ * @param s Vstupný nulovo ukončený reťazec (modifikovateľný).
+ * @return Ukazovateľ na orezaný reťazec (v rámci pôvodného bufferu).
+ **/
+char *trim(char *s);
