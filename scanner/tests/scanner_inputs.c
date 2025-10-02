5b18b20aa269004ddf7204928ce1b1799f575509
Katarína Kužmová <85704088+katewins420@users.noreply.github.com>
2025-10-02 14:46:00 +0200

    Scanner: Number tests.

--- scanner/tests/scanner_inputs.c
+++ scanner/tests/scanner_inputs.c
@@ -9,7 +9,7 @@ typedef struct {
 } LexerTest;
 
 
-// --- TEST 1 --- 
+// --- TEST Simple assignment --- 
 char *test1_name = "Simple assignment";
 char *test1_input = "var x = y + 10\n";
 Token test1_expectation[] = {
@@ -24,7 +24,7 @@ Token test1_expectation[] = {
 int test1_expected_count = 7;
 // ------------
 
-// --- TEST 2 --- 
+// --- TEST Variable names with nums --- 
 char *test2_name = "Variable names with nums";
 char *test2_input = "var x_2 = a1 + 10\n";
 Token test2_expectation[] = {
@@ -39,9 +39,9 @@ Token test2_expectation[] = {
 int test2_expected_count = 7;
 // ------------
 
-// --- TEST 3 --- 
+// --- TEST Whitespace troubleshooting --- 
 char *test3_name = "Whitespace troubleshooting";
-char *test3_input = "   var    x=a +  10\n ";
+char *test3_input = " \tvar  \tx=a +  10\n ";
 Token test3_expectation[] = {
     {TOKEN_VAR, NULL},
     {TOKEN_ID, "x"},
@@ -52,4 +52,53 @@ Token test3_expectation[] = {
     {TOKEN_LINE_END, NULL}
 };
 int test3_expected_count = 7;
+// ------------
+
+// --- TEST Classic Numbers --- 
+char *test4_name = "Classic Numbers";
+char *test4_input = "   var num = 12 +10.25 \n";
+Token test4_expectation[] = {
+    {TOKEN_VAR, NULL},
+    {TOKEN_ID, "num"},
+    {TOKEN_ASSIGN, NULL},
+    {TOKEN_INT_LITERAL, "12"},
+    {TOKEN_PLUS, NULL},
+    {TOKEN_INT_LITERAL, "10.25"},
+    {TOKEN_LINE_END, NULL}
+};
+int test4_expected_count = 7;
+// ------------
+
+// --- TEST Exponential nums --- 
+char *test5_name = "Exponential nums";
+char *test5_input = "var num = 1.12e5 + 0.55E+2 + 0.58e-4 \n";
+Token test5_expectation[] = {
+    {TOKEN_VAR, NULL},
+    {TOKEN_ID, "num"},
+    {TOKEN_ASSIGN, NULL},
+    {TOKEN_INT_LITERAL, "1.12e5"},
+    {TOKEN_PLUS, NULL},
+    {TOKEN_INT_LITERAL, "0.55E+2"},
+    {TOKEN_PLUS, NULL},
+    {TOKEN_INT_LITERAL, "0.58e-4"},
+    {TOKEN_LINE_END, NULL}
+};
+int test5_expected_count = 7;
+// ------------
+
+// --- TEST Hex nums --- 
+char *test6_name = "Hex nums";
+char *test6_input = "var num = 0xdd + 0x1F + 0xAA\n";
+Token test6_expectation[] = {
+    {TOKEN_VAR, NULL},
+    {TOKEN_ID, "num"},
+    {TOKEN_ASSIGN, NULL},
+    {TOKEN_INT_LITERAL, "0xdd"},
+    {TOKEN_PLUS, NULL},
+    {TOKEN_INT_LITERAL, "0x1F"},
+    {TOKEN_PLUS, NULL},
+    {TOKEN_INT_LITERAL, "0xAA"},
+    {TOKEN_LINE_END, NULL}
+};
+int test6_expected_count = 7;
 // ------------
