27288ab978058b380d7d14e8a0cd3b0b89df576e
Katarína Kužmová <85704088+katewins420@users.noreply.github.com>
2025-10-18 22:36:47 +0200

    Scanner: Fix for test num starting with dot.

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -307,6 +307,8 @@ int get_num_format(char *array, TokenType *out_token) {
     unsigned len = strlen(array);
     if(array[0] == '0' && array[1] == '0')
         return 1;
+    if(array[0] == '.')
+        return 1;
     if(len == 4 && array[0] == '0' && (array[1] == 'x' || array[1] == 'X')){
         for(unsigned i = 2; i< strlen(array); i++){
             if(!((array[i] >= 'A' && array[i] <= 'F') || (array[i] >= 'a' && array[i] <= 'f') || (array[i] >= '0' && array[i] <= '9')))
